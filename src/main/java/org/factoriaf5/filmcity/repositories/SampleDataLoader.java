0a650c64bbb4417d9dc8a5839607dd6c397d9df3
Glauciagmm <glauciamesquitagmm@hotmail.com>
2022-04-13 17:20:27 +0200

    correción

--- src/main/java/org/factoriaf5/filmcity/repositories/SampleDataLoader.java
+++ src/main/java/org/factoriaf5/filmcity/repositories/SampleDataLoader.java
@@ -48,7 +48,7 @@ public class SampleDataLoader {
                             true,
                             "Brian de Palma",
                             2),
-            new Movie("El Color Purpura",
+            new Movie("El Color Púrpura",
                             "https://es.web.img3.acsta.net/r_1920_1080/img/01/f9/01f90f60ddc942042c0800df54f830d2.jpg",
                             "Steven Spielberg",
                             1985,
